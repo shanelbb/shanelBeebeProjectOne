83a88de0d3a381a9c50de6344fbf912409483814
Shanel Beebe <shanelbeebe@icloud.com>
2021-03-17 19:26:12 -0600

    added custom alerts to blog and contact page

--- js/formScript.js
+++ js/formScript.js
@@ -1,35 +1,71 @@
-// Form error handling 
-
+// Form handling 
+const alert = document.getElementById('alert');
+const alertContent = document.getElementById('alertContent');
 const submit = document.getElementById('btn');
 const userName = document.getElementById('userName');
 const userEmail = document.getElementById('userEmail');
 const userComment = document.getElementById('userComment');
 
-// Attaches event lister to submit button
-submit.addEventListener('click', (e) => {
-    // prevent page reload
-    e.preventDefault();
-    
-    // Triggers error message if required fields aren't fille out
-    if (!userName.value || !userEmail.value || !userComment.value) {
-        Swal.fire({
-            title: 'Oops!',
-            text: 'Please fill out all required fields',
-            iconColor: '#C3DAB7',
-            background: '#f6f6f6',
-            confirmButtonColor: '#C3DAB7',
-        })
-    } else {
-        //  Success message when form is submitted
-        Swal.fire({
-            text: "Submitted Succesfully",
-            icon: 'success',
-            iconColor: '#C3DAB7',
-            background: '#f6f6f6',
-            confirmButtonColor: '#C3DAB7',
-        }    
-    )
-        document.getElementById('userForm').reset();
+// opens alert box
+openAlert = () => {
+    alert.classList.add('open');
+    window.addEventListener('keyup',closeOnKeyup);
+}
+
+// closes alert box
+closeAlert = () => {
+    alert.classList.remove('open')
+}
+
+// allows user to click outside alert box to close
+handleClickToClose = (e) => {
+    if (e.target === e.currentTarget) {
+        closeAlert()
     }
-        
+}
+
+// allows user to close alert box with Enter key
+closeOnKeyup = (e) => {
+    if (e.key === 'Enter') {
+        closeAlert();
+    }
+}
+
+// attaches close function to dynamically generated alert button
+document.addEventListener('click', (e) => {
+    if (e.target && e.target.id === 'alertBtn') {
+        closeAlert();
+    }
 })
+
+formAlert = () => {
+    // Attaches event lister to submit button
+    submit.addEventListener('click', (e) => {
+        e.preventDefault();
+        // Triggers error message if required fields aren't filled out
+        if (!userName.value || !userEmail.value || !userComment.value) {
+            
+            alertContent.innerHTML = (
+                `<h3>Oops!</h3>
+                <p>Please fill out all required fields</p>
+                <button class="alertBtn" id="alertBtn">OK</button>
+                `
+            )
+            openAlert();
+        } else {
+            //  Success message when form is submitted
+            alertContent.innerHTML = (
+                `
+                <h3>Thank You!</h3>
+                <p>Submitted Successfully</p>
+                <button class"alertBtn" id="alertBtn">OK</button>
+                `
+            )
+            openAlert();
+            document.getElementById('userForm').reset();
+        }
+    })
+}
+
+formAlert();
+alert.addEventListener('click', handleClickToClose)
