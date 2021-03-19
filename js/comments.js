4e49332d5f941c223545b85954075ac2dc1d575b
Shanel Beebe <shanelbeebe@icloud.com>
2021-03-19 08:52:41 -0600

    added some local storage functionality

--- js/comments.js
+++ js/comments.js
@@ -1,42 +1,67 @@
 const commentSection = document.querySelector('.postComments');
+const currentDate = new Date();
+const newDate = currentDate.toUTCString()
+const commentData = {
+    name: userName.value,
+    comment: userComment.value,
+    date: newDate
+}
+
+
 
 document.addEventListener('click', (e) => {
     if (e.target && e.target.className === 'successBtn') {
+        commentData.name = userName.value
+        commentData.comment = userComment.value
+        commentData.date = newDate
+        localStorage.setItem('commentData', JSON.stringify(commentData));
 
-        // create div for comments to live in
-        const commentDiv = document.createElement('div');
-        commentDiv.classList.add('comment')
-        commentSection.appendChild(commentDiv);
-
-        // Add an image to comment
-        const commentImg = document.createElement('img');
-        commentImg.src = "../assets/anon-user.png"
-        commentDiv.appendChild(commentImg)
-
-        // add comment bubble
-        const bubble = document.createElement('div');
-        bubble.classList.add('bubble');
-        commentDiv.appendChild(bubble)
-
-        // add h4 
-        const h4 = document.createElement('h4');
-        const nameVal = userName.value;
-        bubble.appendChild(h4);
-        h4.innerText = nameVal;
-
-        //add p with comment 
-        const commentP = document.createElement('p');
-        const commentVal = userComment.value;
-        bubble.appendChild(commentP)
-        commentP.innerText = commentVal
-
-        //add p with date 
-        const dateP = document.createElement('p');
-        dateP.classList.add('commentDate')
-        bubble.appendChild(dateP);
-        const currentDate = new Date()
-        dateP.innerText = currentDate.toUTCString()
-
-        document.getElementById('userForm').reset();
+        getComments();
     }
-})
+    })
+
+const getComments = () => {
+    const savedComments = JSON.parse(localStorage.getItem('commentData'))
+
+    console.log(savedComments);
+    
+    
+    // create div for comments to live in
+    const commentDiv = document.createElement('div');
+    commentDiv.classList.add('comment')
+    commentSection.appendChild(commentDiv);
+            
+    // Add an image to comment
+    const commentImg = document.createElement('img');
+    commentImg.src = "../assets/anon-user.png"
+    commentDiv.appendChild(commentImg)
+
+    // add comment bubble
+    const bubble = document.createElement('div');
+    bubble.classList.add('bubble');
+    commentDiv.appendChild(bubble)
+            
+    // add h4 
+    const h4 = document.createElement('h4');
+    // const nameVal = savedComments[0];
+    bubble.appendChild(h4);
+    h4.innerText = savedComments.name;
+            
+    //add p with comment 
+    const commentP = document.createElement('p');
+    // const commentVal = savedComments[1];
+    bubble.appendChild(commentP)
+    commentP.innerText = savedComments.comment
+            
+    //add p with date 
+    const dateP = document.createElement('p');
+    dateP.classList.add('commentDate')
+    bubble.appendChild(dateP);
+    dateP.innerText = savedComments.date
+    document.getElementById('userForm').reset();
+
+}
+
+window.onload = function () {
+    getComments();
+}
