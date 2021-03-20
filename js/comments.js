82a3409c7d93a433ff35ff6538b9779183636eb2
Shanel Beebe <shanelbeebe@icloud.com>
2021-03-19 21:02:21 -0600

    worked on persisting comments with local storage

--- js/comments.js
+++ js/comments.js
@@ -1,67 +1,90 @@
 const commentSection = document.querySelector('.postComments');
 const currentDate = new Date();
 const newDate = currentDate.toUTCString()
-const commentData = {
-    name: userName.value,
-    comment: userComment.value,
-    date: newDate
-}
 
+const commentData = []
+
+const assignCommentObj = () => {
+    const newComment = {
+        name: '',
+        comment: '',
+        date: '',
+    }
+    newComment.name = userName.value
+    newComment.comment = userComment.value
+    newComment.date = newDate
+    return newComment
+}
 
 
 document.addEventListener('click', (e) => {
-    if (e.target && e.target.className === 'successBtn') {
-        commentData.name = userName.value
-        commentData.comment = userComment.value
-        commentData.date = newDate
-        localStorage.setItem('commentData', JSON.stringify(commentData));
-
-        getComments();
+    if (e.target && e.target.className === 'successBtn') {          
+        
+        commentData.push(assignCommentObj())
+        
+        console.log(commentData);
+        
+        addCommentLS(commentData)
+        
+        const comments = getCommentLS();
+        getComments(comments)
     }
-    })
+})
 
-const getComments = () => {
-    const savedComments = JSON.parse(localStorage.getItem('commentData'))
-
-    console.log(savedComments);
+function addCommentLS(commentId) {
+    localStorage.setItem('commentsData', JSON.stringify(commentId));
     
-    
-    // create div for comments to live in
-    const commentDiv = document.createElement('div');
-    commentDiv.classList.add('comment')
-    commentSection.appendChild(commentDiv);
-            
-    // Add an image to comment
-    const commentImg = document.createElement('img');
-    commentImg.src = "../assets/anon-user.png"
-    commentDiv.appendChild(commentImg)
+}
 
-    // add comment bubble
-    const bubble = document.createElement('div');
-    bubble.classList.add('bubble');
-    commentDiv.appendChild(bubble)
-            
-    // add h4 
-    const h4 = document.createElement('h4');
-    // const nameVal = savedComments[0];
-    bubble.appendChild(h4);
-    h4.innerText = savedComments.name;
-            
-    //add p with comment 
-    const commentP = document.createElement('p');
-    // const commentVal = savedComments[1];
-    bubble.appendChild(commentP)
-    commentP.innerText = savedComments.comment
-            
-    //add p with date 
-    const dateP = document.createElement('p');
-    dateP.classList.add('commentDate')
-    bubble.appendChild(dateP);
-    dateP.innerText = savedComments.date
-    document.getElementById('userForm').reset();
+function getCommentLS() {
+    const localData = localStorage.getItem('commentsData')
+    
+    return localData ? JSON.parse(localData) : []
+
+    // return savedComments === null ? [] : savedComments;
 
 }
 
+const getComments = (saved) => {
+
+    for (let i = 0; i < saved.length; i++){
+        // create div for comments to live in
+        const commentDiv = document.createElement('div');
+        commentDiv.classList.add('comment')
+        commentSection.appendChild(commentDiv);
+                
+        // Add an image to comment
+        const commentImg = document.createElement('img');
+        commentImg.src = "../assets/anon-user.png"
+        commentDiv.appendChild(commentImg)
+    
+        // add comment bubble
+        const bubble = document.createElement('div');
+        bubble.classList.add('bubble');
+        commentDiv.appendChild(bubble)
+                
+        // add h4 
+        const h4 = document.createElement('h4');
+        // const nameVal = savedComments[0];
+        bubble.appendChild(h4);
+        h4.innerText = saved[i].name;
+                
+        //add p with comment 
+        const commentP = document.createElement('p');
+        // const commentVal = savedComments[1];
+        bubble.appendChild(commentP)
+        commentP.innerText = saved[i].comment
+                
+        //add p with date 
+        const dateP = document.createElement('p');
+        dateP.classList.add('commentDate')
+        bubble.appendChild(dateP);
+        dateP.innerText = saved[i].date
+    }
+
+    document.getElementById('userForm').reset();
+}
+
 window.onload = function () {
-    getComments();
+    getComments(getCommentLS());
 }
